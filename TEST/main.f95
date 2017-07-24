d2607cdda192188a4c7fa32504c57c2836195335
Jochem Hoorneman <j.d.hoorneman@student.tudelft.nl>
2017-07-24 17:37:46 +0200

    Start with new filter

--- TEST/main.f95
+++ TEST/main.f95
@@ -3,17 +3,16 @@
 
 
 program main
+  USE :: testmodule
   IMPLICIT NONE
-  use :: testmodule
   INTEGER :: ans, a, b
     ans = Substraction(1,5)
   write(*,*) ans
-
-contains
-
-INTEGER FUNCTION Addition(a, b)
-    IMPLICIT NONE
-    INTEGER, INTENT(IN) :: a, b
-    Addition = a+b
-END FUNCTION
+CONTAINS
+    INTEGER FUNCTION Addition(a, b)
+        IMPLICIT NONE
+        INTEGER, INTENT(IN) :: a, b
+        Addition = a+b
+    END FUNCTION
 end program
+
--- TEST/testmodule.f90
+++ TEST/testmodule.f90
@@ -1,8 +1,9 @@
+
 module testmodule
-    contains
-FUNCTION Substraction(a, b)
-    IMPLICIT NONE
-    INTEGER, INTENT(IN) :: a, b
-    Substraction = a-b
-END FUNCTION
+CONTAINS
+    FUNCTION Substraction(a, b)
+        IMPLICIT NONE
+        INTEGER, INTENT(IN) :: a, b
+        Substraction = a-b
+    END FUNCTION
 end module
