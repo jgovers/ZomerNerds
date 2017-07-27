7acde22d6998eee70f9bd6b6de27a1f5b4d5725a
jgovers <j.a.govers@gmail.com>
2017-07-27 15:15:58 +0200

    It is possible to pass along the vaiables to the first subroutine and then make new generic ones in the second subroutine

--- TEST/TestRoutine.f90
+++ TEST/TestRoutine.f90
@@ -4,16 +4,28 @@ SUBROUTINE TestRoutine(a,b,c,d,e,f)
 
     REAL(4),INTENT(INOUT)   :: a,b,c,d,e,f
 
-    CALL TestSubRoutine(a,b,c)
-    CALL TestSubRoutine(a,e,f)
+    CALL TestSubroutine()
 
     CONTAINS
 
-        SUBROUTINE TestSubRoutine(h,i)
-            REAL(4),INTENT(INOUT)   :: h,i
+        SUBROUTINE TestSubRoutine()
 
-            i = a*h
+            IMPLICIT NONE
+
+            CALL TestSubSubRoutine(a,b,c)
+            CALL TestSubSubRoutine(d,e,f)
+
 
         END SUBROUTINE TestSubRoutine
+
+        SUBROUTINE TestSubSubRoutine(g,h,i)
+
+            IMPLICIT NONE
+
+            REAL(4),INTENT(INOUT)   :: g,h,i
+
+            i = g*h
+
+        END SUBROUTINE TestSubSubRoutine
 
 END SUBROUTINE TestRoutine
