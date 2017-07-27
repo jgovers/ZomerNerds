77bcbe196b69ce619365a4a342f6d7f7df47306a
jgovers <j.a.govers@gmail.com>
2017-07-27 09:21:35 +0200

    Trying to pull the files apart, not very succesfull, mods dont work

--- DISCON.f90
+++ DISCON.f90
@@ -1,120 +1,3 @@
-!=======================================================================
-!SUBROUTINE DISCON ( avrSWAP, from_SC, to_SC, aviFAIL, accINFILE, avcOUTNAME, avcMSG ) BIND (C, NAME='DISCON')
-
-MODULE Filters
-    CONTAINS
-    	REAL FUNCTION LPFilter(InputSignal,DT,CornerFreq,iStatus)
-            ! Discrete time Low-Pass Filter
-
-            IMPLICIT NONE
-
-            REAL(4), INTENT(IN)     :: InputSignal,DT,CornerFreq    ! DT = time step [s], CornerFreq = corner frequency [rad/s]
-            INTEGER, INTENT(IN)		:: iStatus
-            REAL(4), SAVE           :: InputSignalLast, OutputSignalLast
-
-            IF ( iStatus == 0 )  THEN           ! .TRUE. if we're on the first call to the DLL
-               OutputSignalLast    = InputSignal    ! Initialization of Output
-               InputSignalLast = InputSignal    ! Initialization of previous Input
-            ENDIF
-
-            LPFilter     = (DT*CornerFreq*InputSignal + DT*CornerFreq*InputSignalLast - (DT*CornerFreq-2.0)*OutputSignalLast)/(DT*CornerFreq+2.0)	!Filter output
-
-            InputSignalLast   = InputSignal		!Save input signal for next time step
-            OutputSignalLast  = LPFilter		!Save input signal for next time step
-
-        END FUNCTION LPFilter
-
-        REAL FUNCTION HPFilter(InputSignal,DT,CornerFreq,iStatus)
-        ! Discrete time High-Pass Filter
-
-            IMPLICIT NONE
-
-            REAL(4), INTENT(IN)     :: InputSignal,DT,CornerFreq    ! DT = time step [s], CornerFreq = corner frequency [rad/s]
-            INTEGER, INTENT(IN)		:: iStatus
-            REAL(4), SAVE           :: InputSignalLast, OutputSignalLast
-            REAL(4)                 :: K
-
-            IF ( iStatus == 0 )  THEN				! .TRUE. if we're on the first call to the DLL
-                OutputSignalLast    = InputSignal	! Initialization of Output
-                InputSignalLast = InputSignal    	! Initialization of previous Input
-            ENDIF
-
-            K = 2.0 / DT
-
-            HPFilter = K/(CornerFreq + K)*InputSignal - K/(CornerFreq + K)*InputSignalLast - (CornerFreq - K)/(CornerFreq + K)*OutputSignalLast	!Filter output
-
-            InputSignalLast   = InputSignal			!Save input signal for next time step
-            OutputSignalLast  = HPFilter			!Save input signal for next time step
-
-        END FUNCTION HPFilter
-
-        REAL FUNCTION NotchFilter(InputSignal,DT,Damp,CornerFreq,K,iStatus)
-        ! Discrete time inverted Notch Filter
-
-            IMPLICIT NONE
-
-            REAL(4), INTENT(IN)     :: InputSignal,DT,Damp,CornerFreq,K    ! DT = time step [s], CornerFreq = corner frequency [rad/s]
-            INTEGER, INTENT(IN)		:: iStatus
-            REAL(4), SAVE           :: InputSignalLast1,InputSignalLast2,OutputSignalLast1,OutputSignalLast2
-
-            IF ( iStatus == 0 )  THEN				! .TRUE. if we're on the first call to the DLL
-                OutputSignalLast1   = InputSignal	! Initialization of Output
-                OutputSignalLast2   = InputSignal
-                InputSignalLast1    = InputSignal
-                InputSignalLast2    = InputSignal   !Initialization of previous Input
-
-            ENDIF
-
-            K = 2.0 / DT
-
-            NotchFilter        = 1/(4+2*DT*Damp*CornerFreq+DT**2*CornerFreq**2) * &
-                ( (8-2*DT**2*CornerFreq**2)*OutputSignalLast1 + (-4+2*DT*Damp*CornerFreq-DT**2*CornerFreq**2)*OutputSignalLast2 + &
-                (2*DT*Damp*CornerFreq*K)*InputSignal + (-2*DT*Damp*CornerFreq*K)*InputSignalLast2 )
-
-            InputSignalLast2    = InputSignalLast1
-            InputSignalLast1    = InputSignal			!Save input signal for next time step
-            OutputSignalLast2   = OutputSignalLast1		!Save input signal for next time step
-            OutputSignalLast1   = NotchFilter
-
-        END FUNCTION HPFilter
-
-        REAL FUNCTION SecLPFilter(InputSignal,DT,Damp,CornerFreq,K,iStatus)
-        ! Discrete time second order Low-Pass Filter
-
-            IMPLICIT NONE
-
-            REAL(4), INTENT(IN)     :: InputSignal,DT,Damp,CornerFreq,K    ! DT = time step [s], CornerFreq = corner frequency [rad/s]
-            INTEGER, INTENT(IN)		:: iStatus
-            REAL(4), SAVE           :: InputSignalLast1,InputSignalLast2,OutputSignalLast1,OutputSignalLast2
-
-            IF ( iStatus == 0 )  THEN				! .TRUE. if we're on the first call to the DLL
-                OutputSignalLast1   = InputSignal	! Initialization of Output
-                OutputSignalLast2   = InputSignal
-                InputSignalLast1    = InputSignal
-                InputSignalLast2    = InputSignal   !Initialization of previous Input
-
-            ENDIF
-
-            K = 2.0 / DT
-
-            SecLPFilter        = 1/(4+4*DT*Damp*CornerFreq+DT**2*CornerFreq**2) * &
-                ( (8-2*DT**2*CornerFreq**2)*OutputSignalLast1 + (-4+4*DT*Damp*CornerFreq-DT**2*CornerFreq**2)*OutputSignalLast2 + &
-                (DT**2*CornerFreq**2)*InputSignal + (2*DT**2*CornerFreq**2)*InputSignalLast1 + (DT**2*CornerFreq**2)*InputSignalLast2 )
-
-            InputSignalLast2    = InputSignalLast1
-            InputSignalLast1    = InputSignal			!Save input signal for next time step
-            OutputSignalLast2   = OutputSignalLast1		!Save input signal for next time step
-            OutputSignalLast1   = SecLPFilter
-
-        END FUNCTION SecLPFilter
-
-END MODULE Filters
-
-
-
-
-
-
 SUBROUTINE DISCON ( avrSWAP, aviFAIL, accINFILE, avcOUTNAME, avcMSG ) BIND (C, NAME='DISCON')
 !DEC$ ATTRIBUTES DLLEXPORT :: DISCON
 
@@ -665,41 +548,41 @@ END SUBROUTINE DISCON
 !END SUBROUTINE
 !=======================================================================
 
-SUBROUTINE ColmanTransform(rootMOOP1, rootMOOP2, rootMOOP3, aziAngle, axisDirect, axisQuadr)
-!The Colman or d-q axis transformation transforms the root out of plane bending moments of each turbine blade
-!to a direct axis and a quadrature axis
-
-	IMPLICIT NONE
-
-	REAL(4), INTENT(IN)		:: rootMOOP1, rootMOOP2, rootMOOP3	!root out of plane bending moments of each blade
-	REAL(4), INTENT(IN)		:: aziAngle							!rotor azimuth angle
-	REAL(4), INTENT(OUT)	:: axisDirect, axisQuadr			!direct axis and quadrature axis outputted by this transform
-	REAL(4), PARAMETER		:: PI = 3.14159265359				!mathematical constant pi
-	REAL(4), PARAMETER		:: phi2 = 2/3*PI					!phase difference to second blade
-	REAL(4), PARAMETER		:: phi3 = 4/3*PI					!phase difference to third blade
-
-	axisDirect	= 2/3 * cos(aziAngle)*rootMOOP1 + cos(aziAngle+phi2)*rootMOOP2 + cos(aziAngle+phi3)*rootMOOP3
-	axisQuadr	= 2/3 * cos(aziAngle)*rootMOOP1 + cos(aziAngle+phi2)*rootMOOP2 + cos(aziAngle+phi3)*rootMOOP3
-
-END SUBROUTINE ColmanTransform
-
-SUBROUTINE ColmanTransformInverse(axisDirect, axisQuadr, aziAngle, delta1pAngle, rootMOOP1, rootMOOP2, rootMOOP3)
-!The inverse Colman or d-q axis transformation transforms the direct axis and quadrature axis
-!back to root out of plane bending moments of each turbine blade
-
-	IMPLICIT NONE
-
-	REAL(4), INTENT(IN)		:: axisDirect, axisQuadr			!direct axis and quadrature axis
-	REAL(4), INTENT(IN)		:: aziAngle 						!rotor azimuth angle
-	REAL(4), INTENT(IN)		:: delta1pAngle						!phase shift added to the azimuth angle TODO: better description
-	REAL(4), INTENT(OUT)	:: rootMOOP1, rootMOOP2, rootMOOP3	!root out of plane bending moments of each blade
-
-	REAL(4), PARAMETER		:: PI = 3.14159265359				!mathematical constant pi
-	REAL(4), PARAMETER		:: phi2 = 2/3*PI					!phase difference to second blade
-	REAL(4), PARAMETER		:: phi3 = 4/3*PI					!phase difference to third blade
-
-	rootMOOP1 = cos(aziAngle+delta1pAngle)*axisDirect + sin(aziAngle+delta1pAngle)*axisQuadr
-	rootMOOP2 = cos(aziAngle+delta1pAngle+phi2)*axisDirect + sin(aziAngle+delta1pAngle+phi2)*axisQuadr
-	rootMOOP3 = cos(aziAngle+delta1pAngle+phi3)*axisDirect + sin(aziAngle+delta1pAngle+phi3)*axisQuadr
-
-END SUBROUTINE ColmanTransformInverse
+!SUBROUTINE ColmanTransform(rootMOOP1, rootMOOP2, rootMOOP3, aziAngle, axisDirect, axisQuadr)
+!!The Colman or d-q axis transformation transforms the root out of plane bending moments of each turbine blade
+!!to a direct axis and a quadrature axis
+!
+!	IMPLICIT NONE
+!
+!	REAL(4), INTENT(IN)		:: rootMOOP1, rootMOOP2, rootMOOP3	!root out of plane bending moments of each blade
+!	REAL(4), INTENT(IN)		:: aziAngle							!rotor azimuth angle
+!	REAL(4), INTENT(OUT)	:: axisDirect, axisQuadr			!direct axis and quadrature axis outputted by this transform
+!	REAL(4), PARAMETER		:: PI = 3.14159265359				!mathematical constant pi
+!	REAL(4), PARAMETER		:: phi2 = 2/3*PI					!phase difference to second blade
+!	REAL(4), PARAMETER		:: phi3 = 4/3*PI					!phase difference to third blade
+!
+!	axisDirect	= 2/3 * cos(aziAngle)*rootMOOP1 + cos(aziAngle+phi2)*rootMOOP2 + cos(aziAngle+phi3)*rootMOOP3
+!	axisQuadr	= 2/3 * cos(aziAngle)*rootMOOP1 + cos(aziAngle+phi2)*rootMOOP2 + cos(aziAngle+phi3)*rootMOOP3
+!
+!END SUBROUTINE ColmanTransform
+!
+!SUBROUTINE ColmanTransformInverse(axisDirect, axisQuadr, aziAngle, delta1pAngle, rootMOOP1, rootMOOP2, rootMOOP3)
+!!The inverse Colman or d-q axis transformation transforms the direct axis and quadrature axis
+!!back to root out of plane bending moments of each turbine blade
+!
+!	IMPLICIT NONE
+!
+!	REAL(4), INTENT(IN)		:: axisDirect, axisQuadr			!direct axis and quadrature axis
+!	REAL(4), INTENT(IN)		:: aziAngle 						!rotor azimuth angle
+!	REAL(4), INTENT(IN)		:: delta1pAngle						!phase shift added to the azimuth angle TODO: better description
+!	REAL(4), INTENT(OUT)	:: rootMOOP1, rootMOOP2, rootMOOP3	!root out of plane bending moments of each blade
+!
+!	REAL(4), PARAMETER		:: PI = 3.14159265359				!mathematical constant pi
+!	REAL(4), PARAMETER		:: phi2 = 2/3*PI					!phase difference to second blade
+!	REAL(4), PARAMETER		:: phi3 = 4/3*PI					!phase difference to third blade
+!
+!	rootMOOP1 = cos(aziAngle+delta1pAngle)*axisDirect + sin(aziAngle+delta1pAngle)*axisQuadr
+!	rootMOOP2 = cos(aziAngle+delta1pAngle+phi2)*axisDirect + sin(aziAngle+delta1pAngle+phi2)*axisQuadr
+!	rootMOOP3 = cos(aziAngle+delta1pAngle+phi3)*axisDirect + sin(aziAngle+delta1pAngle+phi3)*axisQuadr
+!
+!END SUBROUTINE ColmanTransformInverse
