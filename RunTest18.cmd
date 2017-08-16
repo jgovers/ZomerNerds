49dc4b1ce04bc37d641d0eb6c874d60ac8c7ac7a
Jochem Hoorneman <j.d.hoorneman@student.tudelft.nl>
2017-08-16 17:52:09 +0200

    These should be able to execute and plot results from Test18 when located inside the FAST directory

---
+++ RunTest18.cmd
@@ -0,0 +1,13 @@
+:: Running Test18
+
+
+:: Change this to the absolute path if this script is not located in \CertTest\5MW_Baseline\ServoData\Source. In that
+:: case also make sure the correct drive is selected 
+set "FASTdir=..\..\..\.."
+
+:: Go to the correct drive
+:: C:
+
+:: Run
+cd %FASTdir%\CertTest
+FAST_Win32.exe Test18.fst
---
+++ plotTest18.m
@@ -0,0 +1,86 @@
+%% plotTest18
+% Plot results from Test18.out. Data is imported in a structure data. Also
+% has the option to run Test18 from the matlab script
+
+%% Cleaning
+close all
+clearvars
+clc
+
+%% Settings
+totalTime = tic;
+testFile = 'Test18.out';      % Name of the test file to read
+runCmdFromHere = false;          % Run the RunTest18.cmd file from this matlab script
+
+%% Loading
+if(runCmdFromHere)  % Run CompileRunAndDebug.cmd and get the correct folder
+    dos('RunTest18.cmd');
+end       
+dataRaw = dlmread([debugFolder testFile],'\t',8,0);
+[~,vars] = size(dataRaw);
+fid = fopen([debugFolder testFile]);
+header = textscan(fid,'%s','delimiter','\t');
+fclose(fid);
+header = strtrim(header{1,1}(4:vars+3));
+for i = 1:vars
+    data.(header{i}) = dataRaw(:,i);
+end
+
+%% Plotting
+% calculate total wind
+Wind1VelTot = sqrt(data.Wind1VelX.^2+data.Wind1VelY.^2+data.Wind1VelZ.^2);
+
+% Plot different wind velocities
+figure;
+title('Wind Velocity')
+hold on
+plot(data.Time,data.Wind1VelX)
+plot(data.Time,data.Wind1VelY)
+plot(data.Time,data.Wind1VelZ)
+plot(data.Time,Wind1VelTot)
+ylabel('Wind velocity [m/s]')
+legend('x','y','z','total')
+
+% Plot rotor and generator speed
+figure;
+s(1) = subplot(2,2,1);
+title('Rotor and Generator Speed')
+hold on
+plot(data.Time,data.RotSpeed)
+ylabel('Rotor speed [rpm]')
+yyaxis right
+plot(data.Time,data.GenSpeed)
+ylabel('Generator speed [rpm]')
+
+% Plot generator torque
+s(2) = subplot(2,2,2);
+title('Generator Torque')
+hold on
+plot(data.Time,data.GenTq)
+ylabel('Torque [kN*m]')
+
+% Plot pitch of blade 1
+s(3) = subplot(2,2,3);
+title('BldPitch1')
+hold on
+plot(data.Time,data.BldPitch1)
+ylabel('Pitch [deg]')
+
+% Calculate RootMtotb1
+RootMtotb1 = sqrt(data.RootMxb1.^2 + data.RootMyb1.^2 + data.RootMzb1.^2);
+
+% Plot the root bending moments of blade 1
+s(4) = subplot(2,2,4);
+title('Blade 1 root bending moments')
+hold on
+plot(data.Time,data.RootMxb1)
+plot(data.Time,data.RootMyb1)
+plot(data.Time,data.RootMzb1)
+plot(data.Time,RootMtotb1)
+ylabel('Moment [kN*m]')
+legend('x','y','z','total')
+
+% Link the time axes of the subplots
+linkaxes(s,'x')
+
+toc(totalTime)
